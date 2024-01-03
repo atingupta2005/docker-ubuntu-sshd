c51c5fb49f1f525007b5a66da5167f09bc7a352c
atingupta2005 <atingupta2005@gmail.com>
2024-01-03 10:01:48 +0530

    Update configure-ssh-user.sh

--- configure-ssh-user.sh
+++ configure-ssh-user.sh
@@ -5,7 +5,7 @@
 : ${PASSWORD:=changeme}
 
 # Create the user with the provided username and set the password
-useradd -ms /bin/bash $SSH_USERNAME && echo "$SSH_USERNAME:$SSH_USERNAME" | chpasswd && adduser $SSH_USERNAME sudo
+useradd -ms /bin/bash $SSH_USERNAME && echo "$SSH_USERNAME:$PASSWORD" | chpasswd && adduser $SSH_USERNAME sudo
 echo "root:root" | chpasswd
 # Set the authorized keys from the AUTHORIZED_KEYS environment variable (if provided)
 if [ -n "$AUTHORIZED_KEYS" ]; then
